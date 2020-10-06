bc95474f211a99b9ecff0d02744d9a2fe3c19cd9
tomad6909 <tomad6909@wrdsb.ca>
2020-10-06 22:11:10 +0000

    Finished A5Q3

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program is meant to calculate the factorial of a positive integer that is entered by the user
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask user to enter a positive integer
+    System.out.println("Please enter an integer to calculate the factorial of");
+    int integer = input.nextInt();
+
+    int answer = 1;
+
+    for(int count = 1; count <= integer; count ++){
+      answer = answer * count;
+    }
+    System.out.print(integer + "! " + "= " + answer);
   }
 }
